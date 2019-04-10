f2c7ab0b15783019754ea11305bbe7a00b0d5fb7
Никита Полуденный <nik_i_ta98@mail.ru>
2019-04-10 22:05:02 +0300

    Окна авторизации и регистрации

---
+++ Training_customer/Login_win.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Training_customer
+{
+	/// <summary>
+	/// Логика взаимодействия для Login_win.xaml
+	/// </summary>
+	public partial class Login_win : Window
+	{
+		public Login_win()
+		{
+			InitializeComponent();
+		}
+
+		private void Tb_log_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			b_ent.IsEnabled = tb_log.Text != "" && tb_pass.Password != "";
+		}
+
+		private void Tb_pass_PasswordChanged(object sender, RoutedEventArgs e)
+		{
+			b_ent.IsEnabled = tb_log.Text != "" && tb_pass.Password != "";
+		}
+
+		private void B_reg_Click(object sender, RoutedEventArgs e)
+		{
+			Reg_win reg_Win = new Reg_win();
+			reg_Win.Show();
+		}
+
+		private void B_ent_Click(object sender, RoutedEventArgs e)
+		{
+			if (/*Неверный логин*/false)
+			{
+				MessageBox.Show("Пользователя с таким логином не существует", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+			}
+			else
+			{
+				if (/*Неверный пароль*/false)
+				{
+					MessageBox.Show("Пара логин-пароль не совпадают", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Warning);
+				}
+				else
+				{
+					/*Переход к главной форме*/
+				}
+			}
+		}
+	}
+}
---
+++ Training_customer/Reg_win.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace Training_customer
+{
+	/// <summary>
+	/// Логика взаимодействия для Reg_win.xaml
+	/// </summary>
+	public partial class Reg_win : Window
+	{
+		string phone = "";
+		public Reg_win()
+		{
+			InitializeComponent();
+		}
+
+		private void tb_text_changed(object sender, TextChangedEventArgs e)
+		{
+			b_reg.IsEnabled = tb_name.Text != "" && tb_sname.Text != "" && tb_pname.Text != "" &&
+				tb_login.Text != "" && tb_pass.Password != "" && tb_repass.Password != "" && new Regex(@"^\d{2}.\d{2}.\d{4}$").IsMatch(date_birth.Text);
+		}
+
+		private void Tb_pass_PasswordChanged(object sender, RoutedEventArgs e)
+		{
+			b_reg.IsEnabled = tb_name.Text != "" && tb_sname.Text != "" && tb_pname.Text != "" &&
+				tb_login.Text != "" && tb_pass.Password != "" && tb_repass.Password != "" && new Regex(@"^\d{2}.\d{2}.\d{4}$").IsMatch(date_birth.Text);
+		}
+
+		private void B_reg_Click(object sender, RoutedEventArgs e)
+		{
+			if (tb_pass.Password == tb_repass.Password)
+			{
+				// TODO
+				// Попытка регистрации
+			}
+			else
+				MessageBox.Show("Пароли не совпадают", "Ошибка подтверждение пароля", MessageBoxButton.OK, MessageBoxImage.Warning);
+		}
+
+		private void Date_birth_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			b_reg.IsEnabled = tb_name.Text != "" && tb_sname.Text != "" && tb_pname.Text != "" &&
+				tb_login.Text != "" && tb_pass.Password != "" && tb_repass.Password != "" && new Regex(@"^\d{2}.\d{2}.\d{4}$").IsMatch(date_birth.Text);
+		}
+
+		private void Tb_phone_PreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			e.Handled = "0123456789".IndexOf(e.Text) < 0 || tb_phone.Text.Length > 9;
+		}
+	}
+}
