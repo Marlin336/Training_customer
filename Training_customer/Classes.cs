57a24de34448650413ee0ab078c5359cd8ae2ad4
Никита Полуденный <nik_i_ta98@mail.ru>
2019-05-27 04:53:51 +0300

    Не тестированные записи в лог и журнал транзакций

--- Training_customer/Classes.cs
+++ Training_customer/Classes.cs
@@ -26,4 +26,42 @@ namespace Training_customer
 			this.sub = sub;
 		}
 	}
+	public class LogList
+	{
+		public int id { get; set; }
+		public int group_id { get; set; }
+		public string date { get; set; }
+		public string in_time { get; set; }
+		public string out_time { get; set; }
+		public string admin { get; set; }
+		public string trainer { get; set; }
+		public int transact_id { get; set; }
+		public LogList(int id, int group_id, string date, string in_time, string out_time, string admin, string trainer, int transact_id)
+		{
+			this.id = id;
+			this.group_id = group_id;
+			this.date = date;
+			this.in_time = in_time;
+			this.out_time = out_time;
+			this.admin = admin;
+			this.trainer = trainer;
+			this.transact_id = transact_id;
+		}
+	}
+	public class TransactList
+	{
+		public int id { get; set; }
+		public string admin { get; set; }
+		public int addition { get; set; }
+		public string date { get; set; }
+		public string time { get; set; }
+		public TransactList(int id, string admin, int addition, string date, string time)
+		{
+			this.id = id;
+			this.admin = admin;
+			this.addition = addition;
+			this.date = date;
+			this.time = time;
+		}
+	}
 }
--- Training_customer/Log_win.xaml.cs
+++ Training_customer/Log_win.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Npgsql;
 
 namespace Training_customer
 {
@@ -24,6 +25,74 @@ namespace Training_customer
         {
             InitializeComponent();
 			this.super = super;
+			FillLogTable();
+			FillTransactTable();
         }
-    }
+
+		private void FillLogTable()
+		{
+			string sql = "select id, group_id, date, in_time, out_time, admin, trainer, transact_id from log_view where customer_id = " + super.user_id;
+			NpgsqlCommand comm = new NpgsqlCommand(sql, super.conn);
+			try
+			{
+				super.conn.Open();
+				NpgsqlDataReader reader = comm.ExecuteReader();
+				for (int i = 0; reader.Read(); i++)
+				{
+					dg_log.Items.Add(new LogList(reader.GetInt32(0), reader.GetInt32(1), reader.GetDate(2).ToString(), reader.GetValue(3).ToString(), reader.GetValue(4).ToString(), reader.GetString(5), reader.GetString(6), reader.GetInt32(7)));
+				}
+			}
+			catch (NpgsqlException ex)
+			{
+				MessageBox.Show(ex.Message, "Ошибка на сервере", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+			}
+			finally { super.conn.Close(); }
+		}
+		private void FillTransactTable()
+		{
+			string sql = "select id, admin, addition, date, time from transact_log_view_cust where customer_id = " + super.user_id;
+			NpgsqlCommand comm = new NpgsqlCommand(sql, super.conn);
+			try
+			{
+				super.conn.Open();
+				NpgsqlDataReader reader = comm.ExecuteReader();
+				for (int i = 0; reader.Read(); i++)
+				{
+					dg_trans.Items.Add(new TransactList(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2), reader.GetDate(3).ToString(), reader.GetValue(4).ToString()));
+				}
+			}
+			catch (NpgsqlException ex)
+			{
+				MessageBox.Show(ex.Message, "Ошибка на сервере", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+			}
+			finally { super.conn.Close(); }
+		}
+
+		private void Dg_log_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+		{
+			if (dg_log.SelectedItem != null)
+			{
+				Dispatcher.BeginInvoke((Action)(() => tab_panel.SelectedItem = tab_transact));
+				LogList selected = dg_log.SelectedItem as LogList;
+				for (int i = 0; i < dg_trans.Items.Count; i++)
+				{
+					TransactList item = dg_trans.Items[i] as TransactList;
+					if (item.id == selected.transact_id)
+					{
+						dg_trans.SelectedIndex = i;
+						dg_trans.ScrollIntoView(dg_trans.Items[i]);
+						break;
+					}
+				}
+			}
+		}
+	}
 }
